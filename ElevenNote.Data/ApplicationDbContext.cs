cf73c53c2470f77a53282ee9eebf5d039ae8e06e
Tony Albacete <tbalbacete@gmail.com>
2022-03-16 15:21:32 -0400

    finished DbContext

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace ElevenNote.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
