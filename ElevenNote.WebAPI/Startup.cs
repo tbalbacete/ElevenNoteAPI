9276e3132d4b0bc8d480764e1e3a93c76e9aa07b
Tony Albacete <tbalbacete@gmail.com>
2022-03-16 15:13:15 -0400

    added DBcontext

--- ElevenNote.WebAPI/Startup.cs
+++ ElevenNote.WebAPI/Startup.cs
@@ -26,6 +26,8 @@ namespace ElevenNote.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add connection string and DbContext setup
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
