769e58e6c51174495c00816ada033e8222379f75
Tony Albacete <tbalbacete@gmail.com>
2022-03-17 09:39:46 -0400

    added user controller

---
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ElevenNote.Services.User;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }   
+    }
+}
