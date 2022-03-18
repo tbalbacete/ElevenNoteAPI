8ac621a12c961d1da7f1fcf9c4dd5640d1ef9c2a
Tony Albacete <tbalbacete@gmail.com>
2022-03-18 10:54:39 -0400

    added create note method to note controller and links to database in note service

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Authorization;
+using ElevenNote.Models.Note;
 
 namespace ElevenNote.WebAPI.Controllers
 {
@@ -19,6 +20,22 @@ namespace ElevenNote.WebAPI.Controllers
         public NoteController(INoteService noteService)
         {
             _noteService = noteService;
+        }
+
+        //Post api/Note
+        public async Task<IActionResult> CreateNote([FromBody] NoteCreate request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(await _noteService.CreateNoteAsync(request))
+            {
+                return Ok("Note created successfully.");
+            }
+
+            return BadRequest("Note could not be created");
         }
 
         //Get api/Note
